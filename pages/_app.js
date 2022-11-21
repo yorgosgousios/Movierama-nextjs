9d5401d3acf02e11c838ba7409bc85f5925c89a9
Yorgos Gousios <yorgos.gousios@gmail.com>
2022-11-21 19:16:38 +0200

    add dependencies in master branch

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,21 @@
 import "../styles/globals.css";
+import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ChakraProvider } from "@chakra-ui/react";
+import { useState } from "react";
+import { ReactQueryDevtools } from "react-query-devtools";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default MyApp;
